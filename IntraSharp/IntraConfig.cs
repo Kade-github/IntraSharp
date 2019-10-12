e6105259709bf049d2ff7e374b1c85ebceed0794
KadePcGames <kadepcgames@gmail.com>
2019-10-12 11:19:18 -0700

    Intial Commit

---
+++ IntraSharp/IntraConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IntraSharp
+{
+    public class IntraConfig
+    {
+        public string ConfigPath { get; set; }
+        public IntraConfiguration Config { get; set; }
+
+        public void SaveConfig()
+        {
+            File.WriteAllText(ConfigPath, JsonConvert.SerializeObject(Config));
+        }
+
+        public static IntraConfig LoadConfig(string configPath)
+        {
+            IntraConfig config = new IntraConfig();
+            config.Config = JsonConvert.DeserializeObject<IntraConfiguration>(File.ReadAllText(configPath));
+            config.ConfigPath = configPath;
+            return config;
+        }
+
+    }
+}
---
+++ IntraSharp/IntraConfiguration.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntraSharp
+{
+    public class IntraConfiguration
+    {
+            [JsonProperty("configVersion")]
+            public long ConfigVersion { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("info")]
+            public string Info { get; set; }
+
+            [JsonProperty("levelResources")]
+            public List<LevelResource> LevelResources { get; set; }
+
+            [JsonProperty("tags")]
+            public string[] Tags { get; set; }
+
+            [JsonProperty("handCount")]
+            public long HandCount { get; set; }
+
+            [JsonProperty("moreInfoURL")]
+            public string MoreInfoUrl { get; set; }
+
+            [JsonProperty("speed")]
+            public long Speed { get; set; }
+
+            [JsonProperty("lives")]
+            public long Lives { get; set; }
+
+            [JsonProperty("maxLives")]
+            public long MaxLives { get; set; }
+
+            [JsonProperty("musicFile")]
+            public string MusicFile { get; set; }
+
+            [JsonProperty("musicTime")]
+            public double MusicTime { get; set; }
+
+            [JsonProperty("iconFile")]
+            public string IconFile { get; set; }
+
+            [JsonProperty("environmentType")]
+            public long EnvironmentType { get; set; }
+
+            [JsonProperty("unlockConditions")]
+            public object[] UnlockConditions { get; set; }
+
+            [JsonProperty("hidden")]
+            public bool Hidden { get; set; }
+
+            [JsonProperty("checkpoints")]
+            public object[] Checkpoints { get; set; }
+
+            [JsonProperty("events")]
+            public List<Event> Events { get; set; }
+        }
+
+        public partial class Event
+        {
+            [JsonProperty("time")]
+            public double Time { get; set; }
+
+            [JsonProperty("data")]
+            public List<string> Data { get; set; }
+        }
+
+        public partial class LevelResource
+        {
+            [JsonProperty("name")]
+            public long Name { get; set; }
+
+            [JsonProperty("type")]
+            public string Type { get; set; }
+
+            [JsonProperty("path")]
+            public string Path { get; set; }
+        }
+
+}
